0dc1bb87909c749a6618f91169a740822839c1b8
yanqiangmiffy <1185918903@qq.com>
2019-11-17 12:02:38 +0800

    update

--- 04_tune_threshold.py
+++ 04_tune_threshold.py
@@ -8,10 +8,17 @@
 @description:
 """
 import pandas as pd
+from argparse import ArgumentParser
+if __name__ == '__main__':
+    parser=ArgumentParser()
+    parser.add_argument('threshold',type=float)
+    args = parser.parse_args()
 
-xgb_prob=pd.read_csv('result/xgb_prob.csv')
+    print(args.threshold)
+    threshold=float(args.threshold)
+    xgb_prob=pd.read_csv('result/xgb_prob.csv')
 
-threshold=0.45
-xgb_prob['target'] = xgb_prob['target'].apply(lambda x:1 if x>threshold else 0)
-print(xgb_prob['target'].value_counts())
-xgb_prob.to_csv('result/xgb_result_{}.csv'.format(threshold), index=False)
+    # threshold=0.45
+    xgb_prob['target'] = xgb_prob['target'].apply(lambda x:1 if x>threshold else 0)
+    print(xgb_prob['target'].value_counts())
+    xgb_prob.to_csv('result/xgb_result_{}.csv'.format(threshold), index=False)
