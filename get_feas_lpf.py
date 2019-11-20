a2dcf3b8f244804937e534330683ddfb270f0b42
李朋飞 <17853530715@163.com>
2019-11-21 00:05:03 +0800

    add lpf feas

--- get_feas_lpf.py
+++ get_feas_lpf.py
@@ -23,7 +23,7 @@ pd.set_option('display.max_columns', None)
 scaler = StandardScaler()
 
 def get_fea(train, test, user, app):
-    test['target'] = 'test'
+    # test['target'] = 'test'
 
     user['mark'] = 0
     user['deviceid_count'] = user.groupby('deviceid').mark.transform('count')
@@ -51,12 +51,11 @@ def get_fea(train, test, user, app):
     app = app.groupby('deviceid')['applist'].apply(lambda x: '|'.join(x)).reset_index()
     app['app_len'] = app['applist'].apply(lambda x: len(x.split('|')))
 
-    df = pd.concat((train, test))
+    df = pd.concat((train, test)).reset_index(drop=True)
     user_duplicate = user.drop_duplicates(subset=['deviceid', 'guid'])
     app_duplicate = app.drop_duplicates(subset=['deviceid'])
     df = pd.merge(df, user_duplicate, on=['deviceid', 'guid'], how='left')
     df = pd.merge(df, app_duplicate, on=['deviceid'], how='left')
-    print(df[df['tag_same'] != 0][['outertag', 'tag', 'tag_same']])
 
     df['ts'] = df['ts'].apply(lambda x: datetime.fromtimestamp(x / 1000))
     df['ts_day'] = df['ts'].apply(lambda x: x.day)
@@ -64,6 +63,21 @@ def get_fea(train, test, user, app):
 
     # 类别特征count特征
     cat_list = [i for i in train.columns if i not in ['id', 'lat', 'lng', 'target', 'timestamp', 'ts']] + ['level']
+
+
+    no_features = ['id', 'target', 'timestamp', 'ID', 'fold']
+
+    lb_feas = ['app_version', 'device_vendor', 'device_version', 'deviceid', 'guid', 'netmodel', 'newsid', 'osversion',
+               'timestamp', 'outertag', 'tag', 'applist', 'ts']
+
+    for fea in lb_feas:
+        print(fea)
+        df[fea].fillna('w', inplace=True)
+        df[fea] = df[fea].astype(str)
+        df[fea] = LabelEncoder().fit_transform(df[fea])
+
+    print(cat_list)
+    print(df[cat_list])
     for i in tqdm(cat_list):
         df['{}_count'.format(i)] = df.groupby(['{}'.format(i)])['id'].transform('count')
 
@@ -81,37 +95,28 @@ def get_fea(train, test, user, app):
             )
         df[i + '_mean_last_1'] = df[i + '_mean_last_1'].astype(float)
 
-    no_features = ['id', 'target', 'timestamp', 'ID', 'fold']
-
-    lb_feas = ['app_version', 'device_vendor', 'device_version', 'deviceid', 'guid', 'netmodel', 'newsid', 'osversion',
-               'timestamp', 'outertag', 'tag', 'applist', 'ts']
-
-    for fea in lb_feas:
-        print(fea)
-        df[fea].fillna('w', inplace=True)
-        df[fea] = df[fea].astype(str)
-        df[fea] = LabelEncoder().fit_transform(df[fea])
-
-
-    train = df[df['target'] != 'test']
-    test = df[df['target'] == 'test']
-
+    # train = df[df['target'] != 'test']
+    # test = df[df['target'] == 'test']
+    train = df[:len(train)]
+    test = df[len(train):]
+    print(train.shape)
+    print(test.shape)
 
     train = train[train['target'].notnull()].reset_index(drop=True)
     print(train.shape)
     return train, test, no_features
 
-train = pd.read_csv('data/train.csv')
-test = pd.read_csv('data/test.csv')
-user = pd.read_csv('data/user.csv')
-app = pd.read_csv('data/app.csv')
-sample = pd.read_csv('data/sample.csv')
+train = pd.read_csv('../data/train.csv')
+test = pd.read_csv('../data/test.csv')
+user = pd.read_csv('../data/user.csv')
+app = pd.read_csv('../data/app.csv')
+sample = pd.read_csv('../data/sample.csv')
 
 train, test, no_features = get_fea(train, test, user, app)
 print('get_fea ok')
 
-label = train['target']
-sub = test[['id']]
+# label = train['target']
+# sub = test[['id']]
 
 features = [fea for fea in train.columns if fea not in no_features]
 
